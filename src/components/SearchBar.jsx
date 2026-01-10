dc03573cc9672038b456dbc2c1ae79b214690952
Md Javed <javedalam1731@gmail.com>
2026-01-10 22:48:18 +0530

    Searchslice setup and reducer setup completed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,29 @@
 import React, { useState } from "react";
 
 const SearchBar = () => {
-  const [text, settext] = useState("");
+  const [text, setText] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(text);
+    setText('')
+  };
 
   return (
     <div>
-      <form className="flex gap-5 p-5">
+      <form
+        onSubmit={(e) => {
+          submitHandler(e);
+        }}
+        className="flex bg-gray-900 gap-5 p-5"
+      >
         <input
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
           required
-          className="border-2 px-4 py-3 text-xl rounded outline-none"
+          className="border-2 w-full px-4 py-3 text-xl rounded outline-none"
           type="text"
           placeholder="Search anything..."
         />
