2259cde7ac3aa31a24d95ecedccec8015ab69302
Md Javed <javedalam1731@gmail.com>
2026-01-11 17:04:09 +0530

    implemented fetch photos, videos and gifs

--- src/components/ResultCard.jsx
+++ src/components/ResultCard.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import { fetchPhotos, fetchVideos, fetchGIF } from "../api/mediaApi";
 const ResultCard = (props) => {
   return (
-    <div className="h-72 gap-3 w-60 bg-amber-300">
-      {props.item.type == 'photos' ? <img className="w-full h-40 object-cover" src={props.item.image} alt="" />: ''}
-      {props.item.type == 'videos' ? <video className="w-full h-40 object-cover" autoPlay loop muted src={props.item.src}></video>: ''}
-      {props.item.type == 'GIF' ? <img className="w-full h-40 object-cover" src={props.item.image} alt="" /> : ''}
-      <h1 className="text-black">{props.item.title}</h1>
+    <div className="h-72 relative gap-3 w-60 bg-white rounded">
+      <div className="h-full ">
+        {props.item.type == 'photos' ? <img className="w-full h-full object-cover" src={props.item.image} alt="" />: ''}
+        {props.item.type == 'videos' ? <video className="w-full h-full object-cover" autoPlay loop muted src={props.item.src}></video>: ''}
+        {props.item.type == 'GIF' ? <img className="w-full h-full object-cover" src={props.item.image} alt="" /> : ''}
+        <div id="btn" className="w-full px-3 py-2 absolute bottom-0">
+            <h2 className="text-sm font-semibold">{props.item.title}</h2>
+        </div>
+      </div>
 
     </div>
   );
