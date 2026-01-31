285e8e88257bf00966c11adee14ebcc76f5eedbf
Md Javed <mdjaved17@users.noreply.github.com>
2026-01-31 23:28:28 +0530

    Setup footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="footer justify-center items-center p-4 bg-gray-800 text-white mt-10">
+      <p className="text-center">&copy; 2026 My App. All rights reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
