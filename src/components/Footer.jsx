b52acaff2a850ec422123054aa54778b34bb15bf
Md Javed <mdjaved17@users.noreply.github.com>
2026-02-02 23:55:59 +0530

    Enhanced Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className="footer justify-center items-center p-4 bg-gray-800 text-white mt-10">
+      <p className=''>Made with ❤️ by Javed</p>
       <p className="text-center">&copy; 2026 My App. All rights reserved.</p>
     </div>
   )
